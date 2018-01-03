d818e7df108537a3da276ea36a0526f8e4a9150d
Duff <you@example.com>
2018-01-03 15:45:01 +0000

    printed hex codes in sdl with new function printCodes()

--- telet.c
+++ telet.c
@@ -5,6 +5,7 @@
 #define RECTSIZE 20
 #define MILLISECONDDELAY 20
 
+void printCodes(SDL_Simplewin *sw, unsigned char hex[25][40], fntrow (*fontdata)[18]);
 void readCodesFromFile(char *filename, unsigned char hex[25][40]);
 FILE *openFile(char *filename, char *mode);
 
@@ -14,39 +15,32 @@ int main(int argc, char **argv)
   unsigned char hex[25][40];
   SDL_Simplewin sw;
   fntrow font[FNTCHARS][FNTHEIGHT];
+  /*
   char* hello;
   hello = "WHERE IS YOUR GOD NOW?";
-
+*/
   printf("STARTING TEST: \n" );
       if ( (argc != 2) )  {
         printf("ERROR: Incorrect usage:\nOnly 1 argument is required.\n");
       }
   readCodesFromFile(argv[1], hex);
+
 printf("XXXXXXXXXX BREAK XXXXXXXXXX\n");
-
-
 Neill_SDL_ReadFont(font, "m7fixed.fnt");
 printf("Font Loaded\n");
 Neill_SDL_Init(&sw);
-Neill_SDL_DrawString(&sw, font, hello, 10, 10);
+
+/*Neill_SDL_DrawString(&sw, font, hello, 10, 10);*/
+printCodes(&sw, hex, font);
 
 
 do{
-
       SDL_Delay(MILLISECONDDELAY);
-
       Neill_SDL_UpdateScreen(&sw);
-
-      /* Has anyone pressed ESC or killed the SDL window ?
-         Must be called frequently - it's the only way of escaping  */
       Neill_SDL_Events(&sw);
-
    }while(!sw.finished);
-
-
    /* Clear up graphics subsystems */
    atexit(SDL_Quit);
-
   return 0;
 }
 
@@ -60,7 +54,6 @@ FILE *openFile(char *filename, char *mode)
   }
   return file;
 }
-
 
 void readCodesFromFile(char *filename, unsigned char hex[25][40])
 {
@@ -76,7 +69,7 @@ void readCodesFromFile(char *filename, unsigned char hex[25][40])
     for (h = 0; h < 25; h++) {
       for (w = 0; w < 40; w++) {
         printf("%02x  ",  temphex[h][w]);
-        hex[h][h] = temphex[h][w];
+        hex[h][w] = temphex[h][w];
         i++;
       }
       printf("\n");
@@ -84,3 +77,29 @@ void readCodesFromFile(char *filename, unsigned char hex[25][40])
   fclose ( fp);
 
   }}
+
+
+
+
+  void printCodes(SDL_Simplewin *sw, unsigned char hex[25][40], fntrow (*fontdata)[18])
+  {
+    int  w, h;
+    unsigned char code[10];
+    w = 0; h = 0;
+
+
+      for (h = 0; h < 25; h++) {
+        for (w = 0; w < 40; w++) {
+          sprintf(( char*)code, "%02x", hex[h][w]);
+          Neill_SDL_DrawString(sw, fontdata, code, w*30, h*20);
+          printf("%s ",(char*)code );
+          /*
+          printf("%02x ",hex[h][w] );
+          Neill_SDL_DrawChar(sw, fontdata,  &code, (w*20), (h*20));
+          */
+
+
+        }
+        printf("\n");
+    }
+    }
