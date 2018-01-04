c5d3ff983e79c65f227ebcaf3efe391ea2c5162d
Duff <you@example.com>
2018-01-04 17:38:28 +0000

    Conversion to ASCII now in PrintCodes() function. Display works for symbols, letters and numbers now. Formatting actually looks okay.

--- telet.c
+++ telet.c
@@ -4,6 +4,9 @@
 
 #define RECTSIZE 20
 #define MILLISECONDDELAY 20
+#define HT 25
+#define WT 40
+#define ON_ERROR(STR) fprintf(stderr, STR ); exit(EXIT_FAILURE)
 
 void printCodes(SDL_Simplewin *sw, unsigned char hex[25][40], fntrow (*fontdata)[18]);
 void readCodesFromFile(char *filename, unsigned char hex[25][40]);
@@ -12,7 +15,7 @@ FILE *openFile(char *filename, char *mode);
 
 int main(int argc, char **argv)
 {
-  unsigned char hex[25][40];
+  unsigned char hex[HT][WT];
   SDL_Simplewin sw;
   fntrow font[FNTCHARS][FNTHEIGHT];
   /*
@@ -34,6 +37,10 @@ Neill_SDL_Init(&sw);
 printCodes(&sw, hex, font);
 
 
+printf("print s\n" );
+
+
+
 do{
       SDL_Delay(MILLISECONDDELAY);
       Neill_SDL_UpdateScreen(&sw);
@@ -43,7 +50,6 @@ do{
    atexit(SDL_Quit);
   return 0;
 }
-
 
 
 FILE *openFile(char *filename, char *mode)
@@ -59,14 +65,18 @@ void readCodesFromFile(char *filename, unsigned char hex[25][40])
 {
   FILE *fp = openFile(filename, "rb");
   int  i, bytes, w, h;
-  unsigned char temphex[25][40];
+  unsigned char temphex[HT][WT];
   i = 1; w = 0; h = 0; bytes = 0;
 
   printf("file open\n");
   while (( bytes = fread ( &temphex, sizeof(unsigned char) ,1000, fp)) > 0) {
-    for (h = 0; h < 25; h++) {
-      for (w = 0; w < 40; w++) {
+    for (h = 0; h < HT; h++) {
+      for (w = 0; w < WT; w++) {
+        /*
         printf("%02x  ",  temphex[h][w]);
+        printf("%d",  temphex[h][w]);
+        */
+        printf("%c",(temphex[h][w]) - 128);
         hex[h][w] = temphex[h][w];
         i++;
       }
@@ -77,17 +87,31 @@ void readCodesFromFile(char *filename, unsigned char hex[25][40])
   }}
 
 
-  void printCodes(SDL_Simplewin *sw, unsigned char hex[25][40], fntrow (*fontdata)[18])
+void printCodes(SDL_Simplewin *sw, unsigned char hex[HT][WT], fntrow (*fontdata)[18])
   {
     int  w, h;
     unsigned char code[10];
     w = 0; h = 0;
-      for (h = 0; h < 25; h++) {
-        for (w = 0; w < 40; w++) {
+    printf("Command: Print Codes:\n" );
+      for (h = 0; h < HT; h++) {
+        for (w = 0; w < WT; w++) {
+          /*
           sprintf(( char*)code, "%02x", hex[h][w]);
-          Neill_SDL_DrawString(sw, fontdata, code, w*30, h*20);
-          printf("%s ",(char*)code );
+          */
+          sprintf(( char*)code, "%c", (hex[h][w])-128);
+
+          Neill_SDL_DrawString(sw, fontdata, (char*)code, w*WT/2, h*HT);
+          printf("%s ",code );
         }
         printf("\n");
-    }
-    }
+  }
+}
+/*
+convertHex(unsigned char hex[HT][WT]){
+
+
+
+
+
+}
+*/
