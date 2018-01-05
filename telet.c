8282e7961b6b16a7cb917e1bb337537323f5c477
Duff <you@example.com>
2018-01-05 16:39:56 +0000

    Friday night: Managed to put codes into 2d array of structs.

--- telet.c
+++ telet.c
@@ -1,39 +1,22 @@
 #include "telet.h"
 #include "stdio.h"
-#include "neillsdl2.h"
-
-#define RECTSIZE 20
-#define MILLISECONDDELAY 20
-#define HT 25
-#define WT 40
-#define ON_ERROR(STR) fprintf(stderr, STR ); exit(EXIT_FAILURE)
-
-void printCodes(SDL_Simplewin *sw, unsigned char hex[25][40], fntrow (*fontdata)[18]);
-void readCodesFromFile(char *filename, unsigned char hex[25][40]);
-FILE *openFile(char *filename, char *mode);
-cell* cell_init(unsigned char data);
 
 int main(int argc, char **argv)
 {
-  unsigned char hex[HT][WT];
+  cell hex[HT][WT];
   SDL_Simplewin sw;
   fntrow font[FNTCHARS][FNTHEIGHT];
-  /*
-  char* hello;
-  hello = "WHERE IS YOUR GOD NOW?";
-*/
+
   printf("STARTING TEST: \n" );
       if ( (argc != 2) )  {
         printf("ERROR: Incorrect usage:\nOnly 1 argument is required.\n");
       }
-  readCodesFromFile(argv[1], hex);
 
-printf("XXXXXXXXXX BREAK XXXXXXXXXX\n");
+importCodes(argv[1], hex);
 Neill_SDL_ReadFont(font, "m7fixed.fnt");
-printf("Font Loaded\n");
 Neill_SDL_Init(&sw);
-/*Neill_SDL_DrawString(&sw, font, hello, 10, 10);*/
 printCodes(&sw, hex, font);
+
 printf("print s\n" );
 
 do{
@@ -41,9 +24,7 @@ do{
       Neill_SDL_UpdateScreen(&sw);
       Neill_SDL_Events(&sw);
   }
-
   while(!sw.finished);
-
       atexit(SDL_Quit);
       return 0;
 }
@@ -58,83 +39,50 @@ FILE *openFile(char *filename, char *mode)
   return file;
 }
 
-void readCodesFromFile(char *filename, unsigned char hex[25][40])
+void importCodes(char *filename, cell hex[25][40])
 {
   FILE *fp = openFile(filename, "rb");
-  int  i, bytes, w, h;
+  int bytes, w, h;
   unsigned char temphex[HT][WT];
-  i = 1; w = 0; h = 0; bytes = 0;
+ w = 0; h = 0; bytes = 0;
 
   printf("file open\n");
   while (( bytes = fread ( &temphex, sizeof(unsigned char) ,1000, fp)) > 0) {
-    for (h = 0; h < HT; h++) {
-      for (w = 0; w < WT; w++) {
-        /*
-        printf("%02x  ",  temphex[h][w]);
-        printf("%d",  temphex[h][w]);
-        */
-        printf("%c",(temphex[h][w]) - 128);
-        hex[h][w] = temphex[h][w];
 
-        /*
+        for (h = 0; h < HT; h++) {
+          for (w = 0; w < WT; w++) {
+            hex[h][w].code = (temphex[h][w]-128);
 
-        hex[h][w] =  cell_init(temphex[h][w]);
-        */
-        i++;
       }
-      printf("\n");
   }
   fclose ( fp);
   }}
 
 
-void printCodes(SDL_Simplewin *sw, unsigned char hex[HT][WT], fntrow (*fontdata)[18])
+void changeFlags(flags *flag)
+  {
+    flag->frontcolor = white;
+    flag->backcolor = black;
+  }
+
+  void setCellFlags(cell *c, flags *flag)
+  {
+    c->flag.frontcolor = flag->frontcolor;
+    c->flag.backcolor = flag->backcolor;
+  }
+
+
+void printCodes(SDL_Simplewin *sw, cell hex[HT][WT], fntrow (*fontdata)[18])
   {
     int  w, h;
-    unsigned char code[10];
     w = 0; h = 0;
+
     printf("Command: Print Codes:\n" );
       for (h = 0; h < HT; h++) {
+
         for (w = 0; w < WT; w++) {
-          /*
-          sprintf(( char*)code, "%02x", hex[h][w]);
-          */
-          sprintf(( char*)code, "%c", (hex[h][w])-128);
-          Neill_SDL_DrawString(sw, fontdata, (char*)code, w*WT/2, h*HT);
-          printf("%s ",code );
+          Neill_SDL_DrawChar(sw, fontdata, hex[h][w].code, w*WT/2, h*HT);
         }
         printf("\n");
   }
 }
-
-cell* cell_init(unsigned char data)
-{
-    cell* c = calloc(1, sizeof(cell));
-    if (c == NULL){
-      return 0;
-    }
-
-    /*c->flag->textcolor = NULL;*/
-    c->data = data;
-    return c;
-}
-
-/*
-void data_insert(cell* c, unsigned char ch)
-{
-
-    if(c->data == NULL) {
-      c->data = ch;
-      return;
-      }
-}
-
-
-convertHex(unsigned char hex[HT][WT]){
-
-
-
-
-
-}
-*/
--- telet.h
+++ telet.h
@@ -2,19 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "neillsdl2.h"
 
+#define RECTSIZE 20
+#define MILLISECONDDELAY 20
+#define HT 25
+#define WT 40
+#define ON_ERROR(STR) fprintf(stderr, STR ); exit(EXIT_FAILURE)
 
 typedef enum color {
   white, black, red, yellow, blue
 } color;
 
 struct flags {
-  color      textcolor;
+  color     frontcolor;
+  color     backcolor;
 } state;
 
 typedef struct flags flags;
 
 typedef struct cell {
   struct flags flag;
-  unsigned char data;
+  unsigned char code;
 } cell;
+
+
+void printCodes(SDL_Simplewin *sw, cell hex[25][40], fntrow (*fontdata)[18]);
+void importCodes(char *filename, cell hex[25][40]);
+void changeFlags(flags *flag);
+FILE *openFile(char *filename, char *mode);
+cell* cell_init(unsigned char data);
