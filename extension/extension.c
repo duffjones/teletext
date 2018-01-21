7f5a954ad58679649746b99dd64803dd16a6dbc7
Duff <you@example.com>
2018-01-21 14:56:22 +0000

    Finished html conversion

--- extension/extension.c
+++ extension/extension.c
@@ -18,54 +18,51 @@ int main(int argc, char **argv)
 
 }
 
-
-
 void printHex(cell hex[25][40], flags* current){
   int  w, h;
   sixels sixel;
   FILE *file  = fopen("testhtml.m7", "w");
   w = 0;h = 0;
 
-
   fprintf(file, "<table style=\"color: white; font-size: 14pt; width: 5%%;cellspacing=\"0\" cellpadding=\"0\";\" bgcolor=\"#000000\">\n");
   fprintf(file, "<tbody>");
 
           for (h = 0; h < HT; h++) {
             changeFlags(current);
-             printf("<tr>\n" );
              fprintf(file, "<tr>\n");
              for (w = 0; w < WT; w++) {
                setFlags(hex[h][w].code, current);
                setCellFlags(&hex[h][w], current);
                setHold(&hex[h][w]);
 
-
-
 if(hex[h][w].flag.mode == contiguous &&
   ((hex[h][w].code > a0 && hex[h][w].code<=bf)|| hex[h][w].code >= e0))
    {
      htmlSixel(&hex[h][w], h,  w, sixel, file);
-/*
-   fprintf(file, "   <td><svg width=\"16\" height=\"18\"><rect width=\"16\" height=\"18\" style=\"fill:%s;\" /> </svg> </td>\n",colorEnum(hex[h][w].flag.frontcolor) );
-*/
    }
-
    else if(hex[h][w].flag.mode == separate &&
-     ((hex[h][w].code >= a0 && hex[h][w].code<=bf)|| hex[h][w].code >= e0))
+     ((hex[h][w].code > a0 && hex[h][w].code<=bf)|| hex[h][w].code >= e0))
       {
       fprintf(file, "   <td><svg width=\"10\" height=\"12\"><rect width=\"10\" height=\"12\" style=\"fill:%s;\" /> </svg> </td>\n",colorEnum(hex[h][w].flag.frontcolor) );
       }
-                 else if(hex[h][w].code <= a0 || hex[h][w].code >= 0xff){
-                 printf("   <td>  </td>\n", hex[h][w].code);
-                 fprintf(file, "   <td style=\"background-color: %s;\">  </td>\n" , colorEnum(hex[h][w].flag.backcolor));
-                  }
 
-                 else {
-                     printf("   <td bgcolor=\"%s\">%c</td>\n", colorEnum(hex[h][w].flag.backcolor), hex[h][w].code -DIFF );
+                  else if (h>2 &&hex[h-1][w].flag.fontsize == topfont)
+                     {
+                     current->fontsize = bottomfont;
+                     fprintf(file, "   <td style=\" background-image: linear-gradient(to top, %s, %s 50%%, %s 50%%, %s); height: 18; font-size: 28px; color: %s;\">%c</td>\n", colorEnum(hex[h][w].flag.backcolor),colorEnum(hex[h][w].flag.backcolor), colorEnum(hex[h-1][w].flag.backcolor), colorEnum(hex[h-1][w].flag.backcolor), colorEnum(hex[h][w].flag.frontcolor) , hex[h][w].code - DIFF);
+
+                     }
+
+                  else if (hex[h][w].flag.fontsize == topfont ||  hex[h][w].code == 0x86)
+                     {
+                       fprintf(file, "<td></td>\n");
+                     }
+
+
+                 else if (hex[h][w].flag.fontsize == normalfont  ){
                      fprintf(file, "   <td style=\"color: %s; background-color: %s;\">%c</td>\n", colorEnum(hex[h][w].flag.frontcolor) , colorEnum(hex[h][w].flag.backcolor)  , hex[h][w].code - DIFF );
                       }
             }
-        printf("</tr>\n" );
         fprintf(file, "</tr>\n");
 }
   fprintf(file, "</tbody>");
@@ -120,26 +117,6 @@ fprintf(file, "  <td>\n <svg width=\"16\" height=\"18\">\n");
   fprintf(file, "</svg></td>\n");
       resetSixels(&sixel);
   }
-
-
-/*
-<svg width="16" height="18">
-<rect width="8" height="6" style="fill:rgb(0,255,255);stroke:rgb(0,0,0)" />
-<rect width="8" height="6" x="8" y="6" style="fill:rgb(0,0,255);stroke-width:3 stroke:rgb(0,0,0)" />
-<rect width="8" height="6" x="0" y="12" style="fill:rgb(0,0,255);stroke-width:3 stroke:rgb(0,0,0)" />
-
-</svg>
-*/
-
-
-
-
-
-
-
-
-
-
 
 const char* colorEnum(enum color color)
 {
